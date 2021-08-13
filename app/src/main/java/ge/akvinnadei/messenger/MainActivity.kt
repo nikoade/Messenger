933a40d5c7943ae6aa14f509ff74039090f03c86
akvin18 <58046767+akvin18@users.noreply.github.com>
2021-08-13 18:18:55 +0400

    Connected to firebase

--- app/src/main/java/ge/akvinnadei/messenger/MainActivity.kt
+++ app/src/main/java/ge/akvinnadei/messenger/MainActivity.kt
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.View.GONE
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.ktx.initialize
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -15,5 +18,10 @@ class MainActivity : AppCompatActivity() {
             clRegister.visibility= View.VISIBLE;
         }
         //test
+        Firebase.initialize(this)
+        val database = Firebase.database
+        val myRef = database.getReference("message")
+
+        myRef.setValue("aeee")
     }
 }
